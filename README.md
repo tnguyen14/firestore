ba1eb2ffaad2b2b499a2050d3e26525c2d1ee93c
Tri Nguyen <tri@tridnguyen.com>
2020-04-10 01:06:46 -0400

    Rely on default GOOGLE_APPLICATION_CREDENTIALS

--- README.md
+++ README.md
@@ -8,13 +8,17 @@ This package assumes the following environment variables to always be set:
 
 ```shell
 FIRESTORE_PROJECT_ID=<your-google-cloud-project-id>
-SERVICE_ACCOUNT_JSON=<path-to-service-account-json-file>
-# instructions for service account https://cloud.google.com/docs/authentication/getting-started
+GOOGLE_APPLICATION_CREDENTIALS=<path-to-service-account-json-file>
+# instructions for service account
+# https://cloud.google.com/docs/authentication/getting-started
 ```
 
 ## Delete collection
 
-The main reason for this package to exist is the implementation of the `deleteCollection` method, which retrieve all documents within the collection or subcollection and delete them, in batches. Inspired by <https://firebase.google.com/docs/firestore/manage-data/delete-data#collections>.
+The main reason for this package to exist is the implementation of
+the `deleteCollection` method, which retrieve all documents within the
+collection or subcollection and delete them, in batches.
+Inspired by <https://firebase.google.com/docs/firestore/manage-data/delete-data#collections>.
 
 ```js
 const { firestore } = require('@tridnguyen/firestore');
@@ -22,7 +26,7 @@ const { firestore } = require('@tridnguyen/firestore');
 const myDocRef = firestore.doc('docs/my-doc');
 
 firestore.deleteCollection('docs/my-doc/sub-collection')
-	.then(() => {
-		return myDocRef.delete();
-	});
+  .then(() => {
+    return myDocRef.delete();
+  });
 ```
--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const Firestore = require('@google-cloud/firestore');
 const firestore = new Firestore({
-	projectId: process.env.FIRESTORE_PROJECT_ID,
-	keyFilename: process.env.SERVICE_ACCOUNT_JSON
+  projectId: process.env.FIRESTORE_PROJECT_ID
 });
 
 firestore.settings({
-	timestampsInSnapshots: true
+  timestampsInSnapshots: true
 });
 
 module.exports = firestore;
@@ -19,47 +18,47 @@ module.exports.batchSize = FIRESTORE_BATCH_SIZE;
 // 1. use global firestore instance
 // 2. fix the use of batchSize (which seems incorrect in the example
 module.exports.deleteCollection = function deleteCollection(
-	collectionPath
+  collectionPath
 ) {
-	var collectionRef = firestore.collection(collectionPath);
-	var query = collectionRef.orderBy('__name__');
+  var collectionRef = firestore.collection(collectionPath);
+  var query = collectionRef.orderBy('__name__');
 
-	return new Promise((resolve, reject) => {
-		deleteQueryBatch(query, resolve, reject);
-	});
+  return new Promise((resolve, reject) => {
+    deleteQueryBatch(query, resolve, reject);
+  });
 };
 
 function deleteQueryBatch(query, resolve, reject) {
-	query
-		.limit(FIRESTORE_BATCH_SIZE)
-		.get()
-		.then(snapshot => {
-			// When there are no documents left, we are done
-			if (snapshot.size === 0) {
-				return 0;
-			}
+  query
+    .limit(FIRESTORE_BATCH_SIZE)
+    .get()
+    .then(snapshot => {
+      // When there are no documents left, we are done
+      if (snapshot.size === 0) {
+        return 0;
+      }
 
-			// Delete documents in a batch
-			var batch = firestore.batch();
-			snapshot.docs.forEach(doc => {
-				batch.delete(doc.ref);
-			});
+      // Delete documents in a batch
+      var batch = firestore.batch();
+      snapshot.docs.forEach(doc => {
+        batch.delete(doc.ref);
+      });
 
-			return batch.commit().then(() => {
-				return snapshot.size;
-			});
-		})
-		.then(numDeleted => {
-			if (numDeleted === 0) {
-				resolve();
-				return;
-			}
+      return batch.commit().then(() => {
+        return snapshot.size;
+      });
+    })
+    .then(numDeleted => {
+      if (numDeleted === 0) {
+        resolve();
+        return;
+      }
 
-			// Recurse on the next process tick, to avoid
-			// exploding the stack.
-			process.nextTick(() => {
-				deleteQueryBatch(query, resolve, reject);
-			});
-		})
-		.catch(reject);
+      // Recurse on the next process tick, to avoid
+      // exploding the stack.
+      process.nextTick(() => {
+        deleteQueryBatch(query, resolve, reject);
+      });
+    })
+    .catch(reject);
 }
